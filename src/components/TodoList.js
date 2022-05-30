1985990d4574cd692c48e9568705e421d4b3205e
Chris Timbol <christimbol707@gmail.com>
2022-05-30 13:24:02 -0700

    add edit button functionality

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -8,25 +8,24 @@ function handleRemove(id, setList, list) {
 export default function TodoList() {
     const [input, setInput] = useState("")
     const [list, setList] = useState([])
-
     return (
         <div className="ToDoContainer">
-            <input
-                type="text"
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-            />
-            <button onClick={() => {
+            <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+            <button className="addButton" onClick={() => {
                 setList([...list, input])
                 setInput("")
-            }}>Add
-            </button>
+            }}>Add</button>
 
             <ul>
                 {list.map((e, i) => {
-                    return <li key={i} id={i}>{e}
+                    return <div key={i}>
+                        <li key={i} id={i}> {e}
+                            <button type="button" className="glyphicon glyphicon-edit" onClick={() => {
+                                list.splice(i, 1, input)// splice looks for i index, removes that element from array, replace with input
+                            }}></button>
                             <button type="button" onClick={() => { setList(list.filter((element, index) => index !== i)) }}>x</button>
                         </li>
+                    </div>
                 })}
             </ul>
         </div>
