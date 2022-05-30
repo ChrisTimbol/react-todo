ed6428f1dc2f3328036f05090c8f5749b6eec404
Chris Timbol <christimbol707@gmail.com>
2022-05-29 21:39:19 -0700

    notes for understanding filter

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -28,7 +28,12 @@ export default function TodoList() {
                         key={i}
                         id={i}
                         onClick={() => {
+                            // return everything that doesnt match i
+                            /// return everything when index does not equal i
+                            // in other words keep everything except the one index that matches i 
                             setList(list.filter((element, index) => {
+                                console.log("filter index = " + index)
+                                console.log("i  = " + i)
                                 return index !== i
                             }))
                         }}> {e} </li>
